71887fa030d105f17a4b841f5bc36703ca68c71f
hellomac87 <hellomac87@gmail.com>
2020-04-11 21:43:54 +0900

    1-3. express와 ts-node

--- lecture/back/index.ts
+++ lecture/back/index.ts
@@ -1,12 +1,13 @@
 import * as express from "express";
+import { Request, Response, NextFunction, Application } from "express";
+const app: Application = express();
+const prod: boolean = process.env.NODE_ENV === "production";
 
-const app = express();
-const prod = process.env.NODE_ENV === "production";
-
-app.get("/", (req, res) => {
+app.set("port", prod ? process.env.PORT : 3065);
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("react node-bird 백엔드 정상 동작!");
 });
 
-app.listen(prod ? process.env.PORT : 3065, () => {
-  console.log("express listening port on");
+app.listen(app.get("port"), () => {
+  console.log(`express listening port on ${app.get("port")}`);
 });
